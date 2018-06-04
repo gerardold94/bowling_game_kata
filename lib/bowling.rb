1a147767d8369c8925a1de931b6f095f5bfaa902
Gerardo Lopez Duenas <gerardo.lopez@magmalabs.io>
2018-06-04 12:24:18 -0500

    Adds tests for bowling game and score method. I add a guard clause in score method to only allow 10 frames, also I add two params (roll_index and frame) to allow get score for a specific frame.
    Note:
    In this commit tests fail

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -8,4 +8,8 @@ class Bowling
   def roll(pins)
     @rolls << pins
   end
+
+  def score(roll_index = 0, frame = 1)
+    return 0 if frame > 10
+  end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -16,4 +16,34 @@ describe Bowling do
   def roll_strike
     game.roll(10)
   end
+
+  it 'scores a gutter game' do
+    roll_many 20, 0
+    expect(game.score).to eq 0
+  end
+
+  it 'scores 20 when all pins are 1' do
+    roll_many 20, 1
+    expect(game.score).to eq 20
+  end
+
+  it 'scores one spare' do
+    roll_spare
+    game.roll 3
+    roll_many 17, 0
+    expect(game.score).to eq 16
+  end
+
+  it 'scores one strike' do
+    roll_strike
+    game.roll 3
+    game.roll 4
+    roll_many 16, 0
+    expect(game.score).to be 24
+  end
+
+  it 'scores perfect game' do
+    roll_many 12, 10
+    expect(game.score).to be 300
+  end
 end
